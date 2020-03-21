cb585515ca44397eea672a5f6b0f2a6125dbc776
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-03-21 11:22:11 +0800

    :tada: 初始化提交。生产者消费者问题和虚假唤醒问题

---
+++ src/main/java/JUC/JUC2/TestProductAndConsumer.java
@@ -0,0 +1,107 @@
+package JUC.JUC2;
+
+/**
+ * @author ：ZouJiaHui
+ * @date ：Created in 2020/3/21 10:36
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+
+/**
+ * 生产者和消费者案例
+ */
+public class TestProductAndConsumer {
+
+  public static void main(String[] args) {
+    Clerk clerk = new Clerk();
+
+    Productor pro = new Productor(clerk);
+    Consumer cus = new Consumer(clerk);
+
+    new Thread(pro, "生产者A").start();
+    new Thread(cus, "消费者B").start();
+
+    new Thread(pro, "生产者C").start();
+    new Thread(cus, "消费者D").start();
+  }
+
+}
+
+//店员
+class Clerk {
+
+  private int product = 0;
+
+  //进货
+  public synchronized void get() {//循环次数2
+    while (product >= 1) {//为了避免虚假唤醒问题，应该总是使用 在循环中
+      System.out.println("产品已满！");
+      try {
+        this.wait();
+      } catch (InterruptedException e) {
+
+      }
+    }
+    System.out.println(Thread.currentThread().getName() + " : " + ++product);
+
+    this.notifyAll();
+
+  }
+
+  //卖货
+  public synchronized void sale() {//product=0;循环次数1
+    while (product <= 0) {
+      System.out.println("缺货！");
+      try {
+        this.wait();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+    System.out.println(Thread.currentThread().getName() + " : " + --product);
+    this.notifyAll();
+
+
+  }
+}
+
+//生产者
+class Productor implements Runnable {
+
+  private Clerk clerk;
+
+  public Productor(Clerk clerk) {
+    this.clerk = clerk;
+  }
+
+  @Override
+  public void run() {
+    for (int i = 0; i < 20; i++) {
+
+      try {
+        Thread.sleep(200);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      clerk.get();
+    }
+  }
+}
+
+//消费者
+class Consumer implements Runnable {
+
+  private Clerk clerk;
+
+  public Consumer(Clerk clerk) {
+    this.clerk = clerk;
+  }
+
+  @Override
+  public void run() {
+    for (int i = 0; i < 20; i++) {
+      clerk.sale();
+    }
+  }
+}
