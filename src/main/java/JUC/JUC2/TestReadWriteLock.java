526052dfaf91d4c95468013aaf0203df48935946
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-03-21 19:19:31 +0800

    :tada: 初始化提交。读写锁

---
+++ src/main/java/JUC/JUC2/TestReadWriteLock.java
@@ -0,0 +1,71 @@
+package JUC.JUC2;
+
+/**
+ * @author ：ZouJiaHui
+ * @date ：Created in 2020/3/21 18:50
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import javax.swing.text.MaskFormatter;
+
+/**
+ * 1.ReadWriteLock:读写锁
+ *
+ * 写写/读写 需要“互斥”
+ *
+ * 读读 不需要互斥
+ */
+public class TestReadWriteLock {
+
+  public static void main(String[] args) {
+    ReadWriteLockDemo rw = new ReadWriteLockDemo();
+
+    new Thread(new Runnable() {
+          @Override
+          public void run() {
+            rw.set((int) (Math.random() * 101));
+      }
+    }, "Write:").start();
+
+    for (int i = 0; i < 100; i++) {
+      new Thread(new Runnable() {
+        @Override
+        public void run() {
+          rw.get();
+        }
+      }).start();
+    }
+  }
+}
+
+class ReadWriteLockDemo {
+
+  private int number = 0;
+
+  private ReadWriteLock lock = new ReentrantReadWriteLock();
+
+  //读
+  public void get() {
+    lock.readLock().lock();//上锁
+    try {
+      System.out.println(Thread.currentThread().getName() + " : " + number);
+    } finally {
+      lock.readLock().unlock();
+    }
+  }
+
+  //写
+  public void set(int number) {
+    lock.writeLock().lock();
+    try {
+      System.out.println(Thread.currentThread().getName());
+      this.number = number;
+    } finally {
+      lock.writeLock().unlock();
+    }
+  }
+}
