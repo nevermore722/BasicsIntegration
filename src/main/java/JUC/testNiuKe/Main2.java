ed13d1af8746bc29fd4ba83c3990e5ff35785a3d
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-03-23 23:57:26 +0800

    :tada: 初始化提交。坑爹的验证用例，我认为我没错

--- src/main/java/JUC/testNiuKe/Main2.java
+++ src/main/java/JUC/testNiuKe/Main2.java
@@ -1,5 +1,9 @@
 package JUC.testNiuKe;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @author ：ZouJiaHui
  * @date ：Created in 2020/3/23 23:39
@@ -8,5 +12,17 @@ package JUC.testNiuKe;
  * @version: 1.0
  */
 public class Main2 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    List list = new ArrayList<Integer>();
+    while (scanner.hasNextInt()){
+      int num = scanner.nextInt();
+      list.add(num);
+    }
+    list.stream().distinct().sorted().forEach(System.out::println);
+
+  }
+
 
 }
