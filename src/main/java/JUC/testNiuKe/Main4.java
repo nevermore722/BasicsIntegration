97bea6388cf1e8bc382f6247a513ffbd456074e1
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-03-25 11:24:33 +0800

    :tada: 初始化提交。判断最后一个单词长度加注释

--- src/main/java/JUC/testNiuKe/Main4.java
+++ src/main/java/JUC/testNiuKe/Main4.java
@@ -9,6 +9,16 @@ import java.util.Scanner;
  * @modified By：
  * @version: 1.0
  */
+
+/**
+ * 题目描述
+ * 计算字符串最后一个单词的长度，单词以空格隔开。
+ * 输入描述:
+ * 一行字符串，非空，长度小于5000。
+ *
+ * 输出描述:
+ * 整数N，最后一个单词的长度。
+ */
 public class Main4 {
 
   public static void main(String[] args) {
