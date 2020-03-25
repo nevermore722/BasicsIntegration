3b9b71624b39b93bc4f33c52e8b15cd3420ed8cb
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-03-25 23:00:45 +0800

    :tada: 初始化提交。字符串倒序去重

---
+++ src/main/java/JUC/testNiuKe/Main10.java
@@ -0,0 +1,49 @@
+package JUC.testNiuKe;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * @author ：ZouJiaHui
+ * @date ：Created in 2020/3/25 22:43
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+
+/**
+ * 题目描述
+ * 输入一个int型整数，按照从右向左的阅读顺序，返回一个不含重复数字的新的整数。
+ *
+ * 输入描述:
+ * 输入一个int型整数
+ *
+ * 输出描述:
+ * 按照从右向左的阅读顺序，返回一个不含重复数字的新的整数
+ */
+public class Main10 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    if (scanner.hasNextInt()) {
+      int nextInt = scanner.nextInt();
+      String str = String.valueOf(nextInt);
+
+      char[] chars = str.toCharArray();
+      int index = chars.length;
+      Set set = new HashSet();
+      for (int i = index - 1; i >= 0; i--) {
+        //如果不包含，打印
+        if (!set.contains(chars[i])) {
+          System.out.print(chars[i]);
+        }
+        set.add(chars[i]);
+      }
+    }
+    scanner.close();
+  }
+}
