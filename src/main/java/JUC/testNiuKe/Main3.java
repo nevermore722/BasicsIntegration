615b771e3baf974bf35dbc5585fe4b168d8250ad
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-03-25 17:37:29 +0800

    :tada: 初始化提交。支持多组同时输入

--- src/main/java/JUC/testNiuKe/Main3.java
+++ src/main/java/JUC/testNiuKe/Main3.java
@@ -9,13 +9,27 @@ import java.util.Scanner;
  * @modified By：
  * @version: 1.0
  */
+
+/**
+ * 题目描述
+ * 写出一个程序，接受一个十六进制的数，输出该数值的十进制表示。（多组同时输入 ）
+ *
+ * 输入描述:
+ * 输入一个十六进制的数值字符串。
+ *
+ * 输出描述:
+ * 输出该数值的十进制字符串。
+ */
 public class Main3 {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    String sixTeenNum = scanner.nextLine();
-    String newSixTeenNum = sixTeenNum.substring(2, sixTeenNum.length());
-    long tenNum = Long.parseLong(newSixTeenNum, 16);
-    System.out.println(tenNum);
+    while (scanner.hasNextLine()) {
+      String sixTeenNum = scanner.nextLine();
+      String newSixTeenNum = sixTeenNum.substring(2, sixTeenNum.length());
+      long tenNum = Long.parseLong(newSixTeenNum, 16);
+      System.out.println(tenNum);
+    }
+    scanner.close();
   }
 }
