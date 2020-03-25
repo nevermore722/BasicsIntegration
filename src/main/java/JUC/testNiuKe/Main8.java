044e1587419be2f381629b5a0ad3c817716603a8
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-03-25 18:24:49 +0800

    :tada: 初始化提交。浮点数取近似数值

--- src/main/java/JUC/testNiuKe/Main8.java
+++ src/main/java/JUC/testNiuKe/Main8.java
@@ -12,24 +12,21 @@ import java.util.Scanner;
  */
 
 /**
- * 题目描述
- * 写出一个程序，接受一个正浮点数值，输出该数值的近似整数值。如果小数点后数值大于等于5,向上取整；小于5，则向下取整。
+ * 题目描述 写出一个程序，接受一个正浮点数值，输出该数值的近似整数值。如果小数点后数值大于等于5,向上取整；小于5，则向下取整。
  *
- * 输入描述:
- * 输入一个正浮点数值
+ * 输入描述: 输入一个正浮点数值
  *
- * 输出描述:
- * 输出该数值的近似整数值
+ * 输出描述: 输出该数值的近似整数值
  */
 public class Main8 {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    if (scanner.hasNextFloat()){
+    if (scanner.hasNextFloat()) {
       float nextFloat = scanner.nextFloat();
       BigDecimal bigDecimal = BigDecimal.valueOf(nextFloat);
       //BigDecimal四舍五入
-      System.out.println(bigDecimal.setScale(0,BigDecimal.ROUND_HALF_UP));
+      System.out.println(bigDecimal.setScale(0, BigDecimal.ROUND_HALF_UP));
     }
   }
 
