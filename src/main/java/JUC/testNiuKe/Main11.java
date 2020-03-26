b46cd8f0610d5f041a4cf6eadf8cf54c2eb297e7
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-03-26 11:41:40 +0800

    :tada: 初始化提交。判断含有字符串个数

---
+++ src/main/java/JUC/testNiuKe/Main11.java
@@ -0,0 +1,46 @@
+package JUC.testNiuKe;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * @author ：ZouJiaHui
+ * @date ：Created in 2020/3/26 11:15
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+
+/**
+ * 题目描述
+ * 编写一个函数，计算字符串中含有的不同字符的个数。字符在ACSII码范围内(0~127)，换行表示结束符，不算在字符里。不在范围内的不作统计。
+ *
+ * 输入描述:
+ * 输入N个字符，字符在ACSII码范围内。
+ *
+ * 输出描述:
+ * 输出范围在(0~127)字符的个数。
+ */
+public class Main11 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    if (scanner.hasNextLine()) {
+      String nextLine = scanner.nextLine();
+      //字符串转字符数组
+      char[] chars = nextLine.toCharArray();
+
+      int count = 0;
+      Set set = new HashSet();
+      for (int i = 0; i < chars.length; i++) {
+        set.add(chars[i]);
+      }
+      System.out.println(set.size());
+    }
+    scanner.close();
+  }
+
+}
