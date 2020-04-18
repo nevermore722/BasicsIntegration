a81c76a0e7fa5870c1650976826b139ae5d939d0
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-04-18 22:03:53 +0800

    :tada: 初始化提交。提交一个ListNode类。Definition for singly-linked list.

---
+++ src/main/java/leetcodePractice/ListNode.java
@@ -0,0 +1,18 @@
+package leetcodePractice;
+
+/**
+ * @author ：ZouJiaHui
+ * @date ：Created in 2020/4/18 22:02
+ * @description： ListNode类  Definition for singly-linked list.
+ * @modified By：
+ * @version: 1.0
+ */
+public class ListNode {
+
+  int val;
+  ListNode next;
+
+  ListNode(int x) {
+    val = x;
+  }
+}
