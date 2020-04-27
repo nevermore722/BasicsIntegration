c187933d94792e895b5af0dff329400b4dc92ff3
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-04-27 22:44:44 +0800

    :tada: 初始化提交。58.最后一个单词的长度

---
+++ src/main/java/leetcodePractice/Test58.java
@@ -0,0 +1,35 @@
+package leetcodePractice;
+
+/**
+ * @author ：ZouJiaHui
+ * @date ：Created in 2020/4/27 22:35
+ * @description： 58.最后一个单词的长度
+ * @modified By：
+ * @version: 1.0
+ */
+
+/**
+ * 给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+ *
+ * 如果不存在最后一个单词，请返回 0 。
+ *
+ * 说明：一个单词是指仅由字母组成、不包含任何空格字符的 最大子字符串。
+ *
+ *  
+ *
+ * 示例:
+ *
+ * 输入: "Hello World" 输出: 5
+ */
+public class Test58 {
+
+  public int lengthOfLastWord(String s) {
+    String[] array = null;
+    array = s.split(" ");
+    if (array.length == 0) {
+      return 0;
+    } else {
+      return array[array.length - 1].length();
+    }
+  }
+}
