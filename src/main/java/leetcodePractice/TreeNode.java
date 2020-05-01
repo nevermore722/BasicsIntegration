a9b33b3be7c24a8e48edf0a2452aa114247a8517
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-05-01 23:28:02 +0800

    :tada: 初始化提交。100. 相同的树

---
+++ src/main/java/leetcodePractice/TreeNode.java
@@ -0,0 +1,19 @@
+package leetcodePractice;
+
+/**
+ * @author ：ZouJiaHui
+ * @date ：Created in 2020/5/1 23:20
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+public class TreeNode {
+
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+}
