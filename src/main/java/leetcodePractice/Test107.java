0902582323112704acbabf46edd105a42ebf28c1
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-05-03 23:19:20 +0800

    :tada: 初始化提交。107. 二叉树的层次遍历 II

--- src/main/java/leetcodePractice/Test107.java
+++ src/main/java/leetcodePractice/Test107.java
@@ -1,7 +1,9 @@
 package leetcodePractice;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @author ：ZouJiaHui
@@ -14,49 +16,38 @@ import java.util.List;
 /**
  * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
  *
- * 例如：
- * 给定二叉树 [3,9,20,null,null,15,7],
+ * 例如： 给定二叉树 [3,9,20,null,null,15,7],
  *
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
- * 返回其自底向上的层次遍历为：
+ * 3 / \ 9  20 /  \ 15   7 返回其自底向上的层次遍历为：
  *
- * [
- *   [15,7],
- *   [9,20],
- *   [3]
- * ]
- *
+ * [ [15,7], [9,20], [3] ]
  */
 public class Test107 {
 
   public List<List<Integer>> levelOrderBottom(TreeNode root) {
-
-    List<List<Integer>> list = new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<>();
     if (root == null) {
-      return list;
+      return ans;
     }
-    //深搜
-    dfs(root, 0, list);
-    return list;
-  }
-
-  private void dfs(TreeNode root, int lelve, List<List<Integer>> list) {
-    if (root == null) {
-      return;
+    Queue<TreeNode> q = new LinkedList<>();
+    q.add(root);
+    while (!q.isEmpty()) {
+      List<Integer> tmp = new ArrayList<>();
+      int len = q.size();
+      for (int i = 0; i < len; i++) {
+        TreeNode node = q.poll();
+        tmp.add(node.val);
+        if (node.left != null) {
+          q.add(node.left);
+        }
+        if (node.right != null) {
+          q.add(node.right);
+        }
+      }
+      // 在索引 0 的位置加入一维数组 tmp
+      // 每次新的数组进来都会被放在开始的位置
+      ans.add(0, tmp);
     }
-    if (list.size() <= lelve) {
-      //说明当前层，还没有开始存数据，进行初始化
-      list.add(lelve, new ArrayList<Integer>());
-    }
-    //将当前节点的数据存储到当前层
-    list.get(lelve).add(root.val);
-
-    //继续遍历遍历下一层的数据
-    dfs(root.left, lelve + 1, list);
-    dfs(root.right, lelve + 1, list);
+    return ans;
   }
 }
